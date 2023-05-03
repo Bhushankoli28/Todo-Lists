29d36281ad83e708fa4c41439b9d50a0aa385e43
Bhushan Koli <96105166+Bhushankoli28@users.noreply.github.com>
2023-05-03 19:35:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useState } from "react";
+import TodoList from "./TodoLists";
+
+const App = () =>{
+
+      const [inputList, setInputList] = useState("");
+      const [Items, setItems] = useState([]);
+
+    const itemEvent = (event) => {
+       setInputList(event.target.value);
+
+    };
+
+    const listOfItems = () => {
+      setItems((previousItem) =>{
+        return [...previousItem, inputList];
+      });
+
+      setInputList("")
+
+    };
+
+
+
+  return(
+    <>
+    <div className="main_div">
+      <div className="center_div">
+      <br/>
+      <h1> ToDo List </h1>
+      <br/>
+
+        <input type="text" placeholder="Text-Box" value={inputList} onChange={itemEvent} />
+        <input type="number" placeholder="Date Time Field"/>
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+          {/* <li>{inputList}</li> */}
+          <li>buy bottle</li>
+
+         { Items.map( (itemval) =>{
+            // return <li>{itemval}</li>
+           return <TodoList text = {itemval}
+            
+            />;
+          })
+          }
+
+          
+          </ol>      
+
+      </div>
+
     </div>
-  );
-}
+    
+    </>
+  )
+
+  
+
+
+};
+
+
 
 export default App;
---
+++ src/TodoLists.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TodoList = (props) => {
+
+
+    // <div >
+    //     <div>-</div>
+    return <li>{props.text}</li>
+    {/* </div> */}
+
+
+}
+
+export default TodoList;
